8da3b691403ec76e20b8035d1ddd86d41c34feea
Jacob Ashin <jacob.ashin@custdata.com>
2022-03-24 16:07:46 -0400

    some changes on test

--- TravelApp/TravelApp/Data/Models/SearchedParams.cs
+++ TravelApp/TravelApp/Data/Models/SearchedParams.cs
@@ -3,16 +3,21 @@ namespace TravelApp.Data.Models
 {
     public class SearchedParams
     {
-        [Required]
+        [Required(ErrorMessage = "A valid date is required")]
         public DateOnly? DateRequested { get; set; }
+        [Required(ErrorMessage = "A valid time is required")]
         public TimeOnly? TimeRequested { get; set; }
+        [Required(ErrorMessage = "Please enter a departing address")]
         public string? FromLocationAddress { get; set; }
+        [Required(ErrorMessage = "Please enter a departing city")]
         public string? FromLocationCity { get; set; }
-        [Required, RegularExpression(@"^([0-9]{5})$")]
+        [Required(ErrorMessage = "A valid 5-digit Zip is required"), RegularExpression(@"^([0-9]{5})$")]
         public string? FromLocationAreaCode { get; set; }
+        [Required(ErrorMessage = "Please enter your destination address")]
         public string? ToLocationAddress { get; set; }
+        [Required(ErrorMessage = "Please enter your destination city")]
         public string? ToLocationCity { get; set; }
-        [Required, RegularExpression(@"^([0-9]{5})$")]
+        [Required(ErrorMessage = "A valid 5-digit Zip is required"), RegularExpression(@" ^ ([0-9]{5})$"), ]
         public string? ToLocationAreaCode { get; set; }
     }
 }
