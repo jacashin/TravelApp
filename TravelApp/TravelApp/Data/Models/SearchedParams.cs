5665c3ee32e91431174003990b1c62a866c30d05
Jacob Ashin <jacob.ashin@custdata.com>
2022-03-24 11:25:16 -0400

    added validation

--- TravelApp/TravelApp/Data/Models/SearchedParams.cs
+++ TravelApp/TravelApp/Data/Models/SearchedParams.cs
@@ -1,10 +1,18 @@
-﻿namespace TravelApp.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace TravelApp.Data.Models
 {
     public class SearchedParams
     {
+        [Required]
         public DateOnly? DateRequested { get; set; }
         public TimeOnly? TimeRequested { get; set; }
-        public string? FromLocation { get; set; }
-        public string? ToLocation { get; set; }
+        public string? FromLocationAddress { get; set; }
+        public string? FromLocationCity { get; set; }
+        [Required, RegularExpression(@"^([0-9]{5})$")]
+        public string? FromLocationAreaCode { get; set; }
+        public string? ToLocationAddress { get; set; }
+        public string? ToLocationCity { get; set; }
+        [Required, RegularExpression(@"^([0-9]{5})$")]
+        public string? ToLocationAreaCode { get; set; }
     }
 }
